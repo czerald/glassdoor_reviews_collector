43edc9432ff5d114483070a2cf062a08165078b4
TRENDPH\czeraldg <czerald_guevarra@trendmicro.com>
2017-07-28 16:46:49 +0800

    Created placeholder for classes and settings

---
+++ GlassdoorReviewsCollector/GlassdoorReviewsCollector/Company.cs
@@ -0,0 +1,11 @@
+﻿namespace GlassdoorReviewsCollector
+{
+    class Company
+    {
+        public Company()
+        {
+        }
+
+
+    }
+}
--- GlassdoorReviewsCollector/GlassdoorReviewsCollector/Program.cs
+++ GlassdoorReviewsCollector/GlassdoorReviewsCollector/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace GlassdoorReviewsCollector
 {
@@ -10,6 +8,7 @@ namespace GlassdoorReviewsCollector
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
