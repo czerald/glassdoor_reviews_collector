596e9c33450e00e51776b21abc63761d214f8d5f
Czerald Guevarra <czerald_guevarra@trendmicro.com>
2017-08-02 02:49:14 +0800

    Separated code for searching in page

--- GlassdoorReviewsCollector/GlassdoorReviewsCollector/Program.cs
+++ GlassdoorReviewsCollector/GlassdoorReviewsCollector/Program.cs
@@ -22,48 +22,7 @@ namespace GlassdoorReviewsCollector
                 }
                 else
                 {
-                    logger.Debug("Initializing ChromeDriver");
-                    IWebDriver driver = new ChromeDriver();
 
-                    logger.Debug("Navigating to home page");
-                    driver.Navigate().GoToUrl(Properties.Settings.Default.homepage);
-                    Thread.Sleep(2000);
-
-                    logger.Debug("Finding form elements");
-                    string sCompanyNameSelector = Properties.Settings.Default.company_name_selector;
-                    string sLocationSelector = Properties.Settings.Default.location_selector;
-                    string sSearchButton = Properties.Settings.Default.search_button_selector;
-
-                    IWebElement elemCompanyNameTextbox = driver.FindElement(By.CssSelector(sCompanyNameSelector));
-                    IWebElement elemLocationTextbox = driver.FindElement(By.CssSelector(sLocationSelector));
-                    IWebElement elemSearchButton = driver.FindElement(By.CssSelector(sSearchButton));
-
-                    logger.Debug("Checking if the required elements are loaded in the home page");
-                    if (!elemCompanyNameTextbox.Displayed || !elemLocationTextbox.Displayed || !elemSearchButton.Displayed)
-                    {
-                        logger.Error("One or more elements in page is not displayed.");
-                        logger.Debug("Element " + sCompanyNameSelector + ": " + elemCompanyNameTextbox.Displayed);
-                        logger.Debug("Element " + sLocationSelector + ": " + elemLocationTextbox.Displayed);
-                        logger.Debug("Element " + sSearchButton + ": " + elemSearchButton.Displayed);
-                    }
-                    else
-                    {
-                        if (!String.IsNullOrEmpty(options.Company))
-                        {
-                            logger.Debug("Setting value to company textbox");
-                            elemCompanyNameTextbox.SendKeys(options.Company);
-                        }
-
-                        if (!String.IsNullOrEmpty(options.Location))
-                        {
-                            logger.Debug("Setting value to location textbox");
-                            elemLocationTextbox.SendKeys(options.Location);
-                        }
-
-                        Console.ReadKey();
-                        elemSearchButton.Click();
-                        Thread.Sleep(5000);
-                    }
                 }
                 Console.ReadKey();
             }
---
+++ GlassdoorReviewsCollector/GlassdoorReviewsCollector/SearchPage.cs
@@ -0,0 +1,80 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace GlassdoorReviewsCollector
+{
+    class SearchPage
+    {
+        private readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private IWebDriver driver;
+        private IWebElement elemCompanyNameTextbox;
+        private IWebElement elemLocationTextbox;
+        private IWebElement elemSearchButton;
+
+        public SearchPage()
+        {
+            logger.Debug("Initializing ChromeDriver");
+            driver = new ChromeDriver();
+        }
+
+        public void NavigateHome()
+        {
+            logger.Debug("Navigating to home page");
+            driver.Navigate().GoToUrl(Properties.Settings.Default.homepage);
+            Thread.Sleep(2000);
+        }
+
+        public bool LocateForm()
+        {
+            logger.Debug("Finding form elements");
+            string sCompanyNameSelector = Properties.Settings.Default.company_name_selector;
+            string sLocationSelector = Properties.Settings.Default.location_selector;
+            string sSearchButton = Properties.Settings.Default.search_button_selector;
+
+            elemCompanyNameTextbox = driver.FindElement(By.CssSelector(sCompanyNameSelector));
+            elemLocationTextbox = driver.FindElement(By.CssSelector(sLocationSelector));
+            elemSearchButton = driver.FindElement(By.CssSelector(sSearchButton));
+
+            logger.Debug("Checking if the required elements are loaded in the home page");
+            if (!elemCompanyNameTextbox.Displayed || !elemLocationTextbox.Displayed || !elemSearchButton.Displayed)
+            {
+                logger.Error("One or more elements in page is not displayed.");
+                logger.Debug("Element " + sCompanyNameSelector + ": " + elemCompanyNameTextbox.Displayed);
+                logger.Debug("Element " + sLocationSelector + ": " + elemLocationTextbox.Displayed);
+                logger.Debug("Element " + sSearchButton + ": " + elemSearchButton.Displayed);
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public bool Search(string Location="", string Company="")
+        {
+            if (!String.IsNullOrEmpty(Company) && !String.IsNullOrEmpty(Location))
+            {
+                if (!String.IsNullOrEmpty(Company))
+                {
+                    logger.Debug("Setting value to company textbox");
+                    elemCompanyNameTextbox.SendKeys(Company);
+                }
+
+                if (!String.IsNullOrEmpty(Location))
+                {
+                    logger.Debug("Setting value to location textbox");
+                    elemLocationTextbox.SendKeys(Location);
+                }
+
+                Console.ReadKey();
+                elemSearchButton.Click();
+                Thread.Sleep(5000);
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
